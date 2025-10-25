0c6a4cf947b692c08ea2014da1a89b41ec88a095
Scotty Moon <scotty@hey.com>
2025-10-25 18:24:28 -0500

    upgrading to rails 8.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_08_163239) do
+ActiveRecord::Schema[8.1].define(version: 2025_10_25_223424) do
   create_table "tokens", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "token_hash", null: false
-    t.string "device_name"
     t.datetime "created_at", null: false
+    t.string "device_name"
+    t.string "token_hash", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["token_hash"], name: "index_tokens_on_token_hash", unique: true
     t.index ["user_id"], name: "index_tokens_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "active", default: false, null: false
+    t.datetime "confirmation_sent_at"
+    t.string "confirmation_token"
+    t.datetime "created_at", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
     t.integer "role", default: 5, null: false
-    t.boolean "active", default: false, null: false
-    t.string "confirmation_token"
-    t.datetime "confirmation_sent_at"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
