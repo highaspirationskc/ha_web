5b2aa6d9864cce08e368dbfd1a9a51c425950d0e
scttymn <scotty@hey.com>
2025-12-12 10:51:15 -0600

    Fix TeamType GraphQL resolvers to return User objects

--- app/graphql/types/team_type.rb
+++ app/graphql/types/team_type.rb
@@ -19,5 +19,27 @@ module Types
     field :event_logs, [Types::EventLogType], null: false
     field :total_points, Integer, null: false, description: "Total points for current Olympic season"
     field :total_community_service_hours, Float, null: false, description: "Total approved community service hours for current Olympic season"
+
+    def mentees
+      User.joins(:mentee).where(mentees: { team_id: object.id })
+    end
+
+    def mentors
+      mentor_ids = object.mentees.where.not(mentor_id: nil).pluck(:mentor_id).uniq
+      User.joins(:mentor).where(mentors: { id: mentor_ids })
+    end
+
+    def parents
+      mentee_ids = object.mentees.pluck(:id)
+      User.joins(guardian: :family_members).where(family_members: { mentee_id: mentee_ids }).distinct
+    end
+
+    def members
+      mentees + mentors + parents
+    end
+
+    def event_logs
+      EventLog.joins(user: :mentee).where(mentees: { team_id: object.id })
+    end
   end
 end
