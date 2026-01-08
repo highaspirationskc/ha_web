4485c0a07a4afb51f4df70ef26397710252e1e9b
scttymn <scotty@hey.com>
2026-01-08 10:48:01 -0600

    removing event restrictions on Olympic season query

--- app/graphql/types/olympic_season_type.rb
+++ app/graphql/types/olympic_season_type.rb
@@ -14,14 +14,7 @@ module Types
     field :events, [Types::EventType], null: false, description: "Events for this season (filtered by year from query input)"
 
     def events
-      # Use year from input if provided, otherwise default to current year
-      year = object.current_year || Date.current.year
-
-      # Use service to get date range
-      service = OlympicSeasonService.new(object)
-      date_range = service.date_range(year)
-
-      Event.where(event_date: date_range)
+      Event.all.order(event_date: :asc)
     end
   end
 end
